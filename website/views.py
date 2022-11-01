42b9c06ae435e9aaad43b3f94e7b2dbc62df5462
electrostrike <knguynvitanh@gmail.com>
2022-11-01 16:49:17 +0700

    View timetable correspond to today's date

--- website/views.py
+++ website/views.py
@@ -22,6 +22,8 @@ class ClassInfor:
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
+    totWeeks, rows, cols = (15, 12, 6)
+    arr = [[[' ' for j in range(cols)] for i in range(rows)] for k in range(totWeeks)]
     if request.method == "POST":
         msv = request.form['msv']
         mycursor.execute("SELECT * FROM sinhvien WHERE MSV = (%s)", (msv,))
@@ -36,15 +38,16 @@ def home():
         mycursor.execute("SELECT * FROM dangky WHERE MSV = (%s)", (msv,))
         data = mycursor.fetchall()
         subjectList = []
-        d1 = datetime.strptime("2022/08/26", "%Y/%m/%d")
+        d1 = datetime.strptime("2022/08/29", "%Y/%m/%d")
         d2 = datetime.now()
         curWeek = int((d2 - d1).days / 7)
+        curDay = (d2 - d1).days % 7 
         for row in data:
             HP = row[1]
             LHP = row[2]
             group = row[3]
             note = row[4]
-            mycursor.execute("SELECT * FROM monhoc WHERE Mã_HP = (%s)", (HP,))
+            mycursor.execute("SELECT * FROM monhoc WHERE Mã_HP = (%s)", (HP))
             data2 = mycursor.fetchone()
             LMHName = data2[1]
             TC = data2[2]
@@ -61,8 +64,6 @@ def home():
                     lecturers += "\n"
                 subjectList.append(ClassInfor(HP + ' ' + LHP, LMHName, line[2], TC, note, line[3], line[4],
                                               line[5], line[6] + '-' + line[7], line[8], lecturers))
-        totWeeks, rows, cols = (15, 12, 6)
-        arr = [[['' for j in range(cols)] for i in range(rows)] for k in range(totWeeks)]
         for line in subjectList:
             day = line.day - 2
             start, end = (int(s) for s in line.time.split('-'))
@@ -83,8 +84,8 @@ def home():
                 for week in range(15):
                     for i in range(start - 1, end):
                         arr[week][i][day] = subject + "\n" + subjectName + "\n" + place
-        return render_template('index.html', hello="Hello " + name, timestamp="Tuần hiện tại: " + str(curWeek + 1),
-                               msv=msv, name=name, birthdate=birthdate, class_name=class_name,gender=gender,
-                               birthplace=birthplace, subjectList=subjectList, arr=arr, curWeek=curWeek)
+        return render_template('index.html', hello="Hello " + name + " " + msv, timestamp="Tuần " + str(curWeek + 1) + " Thứ " + str(curDay),
+                               msv=msv, name=name, birthdate=birthdate, class_name=class_name, gender=gender,
+                               birthplace=birthplace, subjectList=subjectList, arr=arr, curWeek=curWeek, curDay=curDay)
 
-    return render_template('index.html')
+    return render_template('index.html', arr=arr)
