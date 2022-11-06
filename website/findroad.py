5732eacbfce639586067dcaba331f0a21f9eef24
Binh <lethanhbinh6203@gmail.com>
2022-11-06 15:30:06 +0700

    Fix posX, posY type

--- website/findroad.py
+++ website/findroad.py
@@ -94,8 +94,8 @@ def initGlobal():
     posX = [0.0]
     posY = [0.0]
     for d in data:
-        posX.append(d[3])
-        posY.append(d[4])
+        posX.append(float(json.dumps(d[3], cls=DecimalEncoder)))
+        posY.append(float(json.dumps(d[4], cls=DecimalEncoder)))
 
 
 def initRoad(showedPlaceList=[], placeNames=[""]):
