405932d806549ee7e9b908080d0af29a7f618994
phuongmt3 <maituphuong3@gmail.com>
2022-11-28 15:25:45 +0700

    test new db

--- website/models.py
+++ website/models.py
@@ -1,10 +1,23 @@
+from dotenv import load_dotenv
+import os
 import mysql.connector
 
+load_dotenv()
+
 db = mysql.connector.connect(
+    host=os.getenv("HOST"),
+    user=os.getenv("USERNAME"),
+    passwd=os.getenv("PASSWORD"),
+    db=os.getenv("DATABASE"),
+    ssl_verify_identity=True,
+    ssl_ca='/etc/pki/tls/certs/ca-bundle.crt'
+)
+
+'''db = mysql.connector.connect(
     host='localhost',
     user='root',
     password='root',
     database='vnu_route_planner_db_new'
-)
+)'''
 
 mycursor = db.cursor()
